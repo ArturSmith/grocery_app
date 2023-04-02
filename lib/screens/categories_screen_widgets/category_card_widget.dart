c4391f20f75b6b38888ce8f96aa721fb3dd5e9d3
Artur Smith <arthur9smith7@gmail.com>
2023-04-02 10:10:38 +0200

    Added animation to categories card

--- lib/screens/categories_screen_widgets/category_card_widget.dart
+++ lib/screens/categories_screen_widgets/category_card_widget.dart
@@ -4,7 +4,7 @@ import 'package:new_project/providers/dark_theme_provider.dart';
 import 'package:new_project/screens/products_screen_widgets/products_screen.dart';
 import 'package:provider/provider.dart';
 
-class CategoryCardWidget extends StatelessWidget {
+class CategoryCardWidget extends StatefulWidget {
   const CategoryCardWidget(
       {super.key,
       required this.categoryName,
@@ -14,60 +14,93 @@ class CategoryCardWidget extends StatelessWidget {
   final Color color;
 
   @override
+  State<CategoryCardWidget> createState() => _CategoryCardWidgetState();
+}
+
+class _CategoryCardWidgetState extends State<CategoryCardWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    duration: const Duration(seconds: 1),
+    vsync: this,
+  );
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _animationController,
+    curve: Curves.linear,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.reverse();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final _isDarkTheme = Provider.of<DarkThemeProvider>(context).getDarkTheme;
     final size = MediaQuery.of(context).size.width;
-    return Stack(
-      children: [
-        InkWell(
-          borderRadius: BorderRadius.circular(20),
-          onTap: (() {
-            Navigator.push(
-                context,
-                PageRouteBuilder(
-                    transitionDuration: const Duration(seconds: 1),
-                    pageBuilder: ((context, animation, secondaryAnimation) =>
-                        ProductsScreen(category: categoryName))));
-          }),
-          child: Container(
-            margin: const EdgeInsets.only(bottom: 13),
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: _isDarkTheme
-                  ? Colors.white.withOpacity(0.8)
-                  : color.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                  color: _isDarkTheme
-                      ? Colors.transparent
-                      : color.withOpacity(0.4),
-                  width: 2),
-            ),
-            child: SvgPicture.asset(
-              image,
-              width: size * 0.4,
-              height: size * 0.4,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
+
+    return ScaleTransition(
+      scale: _animation,
+      child: Stack(
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: (() {
+              Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                      transitionDuration: const Duration(seconds: 1),
+                      pageBuilder: ((context, animation, secondaryAnimation) =>
+                          ProductsScreen(category: widget.categoryName))));
+            }),
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 13),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: _isDarkTheme
+                    ? Colors.white.withOpacity(0.8)
+                    : widget.color.withOpacity(0.1),
                 borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-                border: Border.all(color: Colors.black)),
-            child: Text(
-              categoryName,
-              style: const TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 20,
-                  color: Colors.black),
+                border: Border.all(
+                    color: _isDarkTheme
+                        ? Colors.transparent
+                        : widget.color.withOpacity(0.4),
+                    width: 2),
+              ),
+              child: SvgPicture.asset(
+                widget.image,
+                width: size * 0.4,
+                height: size * 0.4,
+              ),
             ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black)),
+              child: Text(
+                widget.categoryName,
+                style: const TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 20,
+                    color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
