098a8502dd30524b853fd96c9a7589ffc957b9ee
Artur Smith <arthur9smith7@gmail.com>
2023-04-07 00:06:03 +0200

    Deleted pagination

--- lib/widgets/my_swiper.dart
+++ lib/widgets/my_swiper.dart
@@ -1,32 +1,24 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-
-import '../constants/theme_style.dart';
+import 'package:new_project/constants/consts.dart';
 
 class MySwiper extends StatelessWidget {
-  MySwiper({super.key, required this.lisOfImages});
+  const MySwiper({super.key, required this.lisOfImages});
   final List<String> lisOfImages;
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size.width;
+    final screenWigth = Consts.screenSize(context).width;
     return SliverToBoxAdapter(
       child: SizedBox(
-        width: size,
-        height: size * 0.7,
+        width: screenWigth,
+        height: screenWigth * 0.7,
         child: Swiper(
           itemCount: lisOfImages.length,
           itemBuilder: (context, index) {
             return Image.asset(lisOfImages[index], fit: BoxFit.fill);
           },
           autoplay: true,
-          pagination: SwiperPagination(
-              alignment: Alignment.bottomCenter,
-              builder: DotSwiperPaginationBuilder(
-                  size: 10,
-                  activeSize: 10,
-                  color: ThemeStyles.setThemeColor(context, false),
-                  activeColor: ThemeStyles.setThemeColor(context, true))),
         ),
       ),
     );
