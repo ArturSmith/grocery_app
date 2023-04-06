5e16964837e141dec6e76c31f120ea517d09725d
Artur Smith <arthur9smith7@gmail.com>
2023-04-07 00:07:02 +0200

    Deleted signin files and methods

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,12 +1,10 @@
 import 'package:new_project/constants/strings.dart';
-import 'package:new_project/screens/authenticate/wrapper.dart';
 import 'package:new_project/screens/bottom_bar_screen_widgets/botton_bar_screen.dart';
 import 'package:new_project/screens/firs_screen.dart/first_screen.dart';
 
 class Routes {
   static final routes = {
     Str.HOME_SCREEN: (context) => const BottomBarScreen(),
-    Str.WRAPPER: (context) => const Wrapper(),
     Str.FIRST_SCREEN: (context) => const FirstScreen()
   };
 }
