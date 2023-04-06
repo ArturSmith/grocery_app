a7ce487df55d24bdfa76377abbfab686f29e3a13
Artur Smith <arthur9smith7@gmail.com>
2023-04-06 23:39:58 +0200

    Changed system color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:new_project/constants/app_colors.dart';
 import 'package:new_project/widgets/my_app.dart';
 import 'firebase_options.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -13,8 +14,8 @@ void main() async {
 
   // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.black,
-    statusBarColor: Colors.black,
+    systemNavigationBarColor: AppColors.darkThemeBacgroundColor,
+    statusBarColor: AppColors.darkThemeBacgroundColor,
   ));
 
   await Hive.initFlutter();
