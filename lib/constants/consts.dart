f8d4beb4c139298ba6eaec4be94728db75fd0069
Artur Smith <arthur9smith7@gmail.com>
2023-04-08 16:58:05 +0200

    Implemented new constant method for reterning navigator

--- lib/constants/consts.dart
+++ lib/constants/consts.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:new_project/constants/strings.dart';
 import 'package:new_project/providers/dark_theme_provider.dart';
+import 'package:new_project/screens/bottom_bar_screen_widgets/botton_bar_screen.dart';
 import 'package:provider/provider.dart';
 
 class Consts {
@@ -9,5 +11,17 @@ class Consts {
 
   static isDark(BuildContext context) {
     return Provider.of<DarkThemeProvider>(context).getDarkTheme;
+  }
+
+  static navigate(Widget screen, BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: const Duration(milliseconds: 500),
+        pageBuilder: ((_, __, ___) => screen),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(opacity: animation, child: child),
+      ),
+    );
   }
 }
