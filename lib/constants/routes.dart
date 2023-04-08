149213e0818d59347596af35102c067d22f62ee1
Artur Smith <arthur9smith7@gmail.com>
2023-04-08 16:59:31 +0200

    Added new route name

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,10 +1,12 @@
 import 'package:new_project/constants/strings.dart';
 import 'package:new_project/screens/bottom_bar_screen_widgets/botton_bar_screen.dart';
+import 'package:new_project/screens/favorites_screen_widgets/favorites_screen.dart';
 import 'package:new_project/screens/firs_screen.dart/first_screen.dart';
 
 class Routes {
   static final routes = {
     Str.HOME_SCREEN: (context) => const BottomBarScreen(),
-    Str.FIRST_SCREEN: (context) => const FirstScreen()
+    Str.FIRST_SCREEN: (context) => const FirstScreen(),
+    Str.FAVORITES_SCREEN: (context) => const FavoritesScreen()
   };
 }
