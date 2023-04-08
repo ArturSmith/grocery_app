732f6dfe5a782d12418c39fed1827a50048b99ca
Artur Smith <arthur9smith7@gmail.com>
2023-04-08 16:59:16 +0200

    Changes in textDecoration constant

--- lib/constants/my_text_decoration.dart
+++ lib/constants/my_text_decoration.dart
@@ -3,12 +3,15 @@ import 'package:new_project/providers/dark_theme_provider.dart';
 import 'package:provider/provider.dart';
 
 import 'app_colors.dart';
+import 'consts.dart';
 
 class MyTextDecoration {
   static TextStyle titleTextStyle(BuildContext context) {
+    final screenWidth = Consts.screenSize(context).width;
+
     final isDark = Provider.of<DarkThemeProvider>(context).getDarkTheme;
     return TextStyle(
-        fontSize: 25,
+        fontSize: screenWidth * 0.06,
         fontWeight: FontWeight.w700,
         color: !isDark ? Colors.white : AppColors.darkThemeBacgroundColor);
   }
