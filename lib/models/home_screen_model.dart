bcf6a7a1249f0e19e7470ed08fb14fe872aa8dd7
Artur Smith <arthur9smith7@gmail.com>
2023-04-08 17:03:12 +0200

    Implemented methods to get diffrent sorted lists of products.

--- lib/models/home_screen_model.dart
+++ lib/models/home_screen_model.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:new_project/api/api.dart';
+import 'package:new_project/entitis/category.dart';
 import 'package:new_project/entitis/product.dart';
 
 class HomeScreenModel extends ChangeNotifier {
   final api = Api();
-
-  List<Product> products = [];
 
   Future<List<Product>> getProdsWithDiscount() {
     final prods = api.getAllProductsWithDiscount();
     return prods;
   }
 
-  set setProducts(List<Product> products) {
-    this.products = products;
-    notifyListeners();
+  Future<List<Product>> getSeasonalProducts() {
+    final prods = api.getAllSeasonalProducts();
+    return prods;
+  }
+
+  Future<List<Category>> getAllCategories() {
+    final allCategories = api.getAllCategories();
+    return allCategories;
+  }
+
+  getProductsOfCategory(String id) {
+    final products = api.getAllProductsOfCategory(id);
+    return products;
   }
 }
