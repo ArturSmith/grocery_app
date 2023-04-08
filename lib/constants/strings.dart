d84db207e51ea448f7e6b6900c4ec7c15365a6d2
Artur Smith <arthur9smith7@gmail.com>
2023-04-08 16:59:46 +0200

    Added new const strings

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -10,7 +10,8 @@ class Str {
   static const PRICE = "price";
   static const DISCOUNT = "discount";
   static const DB_INSTANCE = "db_Instance";
-  static const BOX = "box";
+  static const BOX_OF_PRODUCTS_ID = "products id";
+  static const FAVORITES = "favorites";
   static const ALL_CAT_AND_PROD = "all_categories_and_products";
   static const ALL_CATEGORIES = "all_categories";
   static const ALL_PRODUCTS = "all_products";
@@ -21,4 +22,5 @@ class Str {
   static const HOME_SCREEN = "/HomeScreen";
   static const WRAPPER = "/Wrapper";
   static const FIRST_SCREEN = "/FirstScreen";
+  static const FAVORITES_SCREEN = "/FavoritesScreen";
 }
