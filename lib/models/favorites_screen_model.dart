0148e4e60bc82163cdabcfa55d303e6ae4ec584a
Artur Smith <arthur9smith7@gmail.com>
2023-04-08 17:00:36 +0200

    Created model for favorites screen

---
+++ lib/models/favorites_screen_model.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
+import 'package:new_project/api/api.dart';
+
+import 'package:new_project/entitis/product.dart';
+
+import '../constants/strings.dart';
+
+class FavoritesScreenModel extends ChangeNotifier {
+  final box = Hive.box(Str.BOX_OF_PRODUCTS_ID);
+  final api = Api();
+
+  List<Product> favoriteProducts = [];
+
+  addProduct(String id) async {
+    if (!box.containsKey(id)) {
+      final product = await api.returnProductByID(id);
+      await box.put(id, id);
+      favoriteProducts.add(product);
+    } else {
+      favoriteProducts.removeWhere((element) => element.id == id);
+      await box.delete(id);
+    }
+    notifyListeners();
+  }
+
+  loadProductsFromHive() async {
+    final listOfIDs = box.values.toList();
+
+    for (var id in listOfIDs) {
+      final product = await api.returnProductByID(id);
+      favoriteProducts.add(product);
+    }
+
+    notifyListeners();
+  }
+}
